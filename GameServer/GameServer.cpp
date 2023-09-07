918f0d305f619096883132eb602753cf3bda9195
Seungjun Lee <beeutiful@naver.com>
2023-09-07 16:40:53 +0900

    스핀락
    . 스핀락 구현 시 lock의 상태를 확인해 주는 부분과 lock 변수의 값을 바꿔주는 부분의 연산이 원자적으로 이뤄저야 한다. 이를 위해 compare_exchange_strong 함수를 사용한다.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,66 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+class SpinLock
+{
+public:
+	void lock() {
+		// CAS (Compare And Swap)
+
+		//if (m_locked == expected) {
+		//	m_locked = desired;
+		//	return true;
+		//}
+		//else {
+		//	expected = m_locked;
+		//	return false;
+		//}
+
+		bool expected = false;
+		const bool desired = true;
+
+		while (m_locked.compare_exchange_strong(expected, desired) == false) 
+		{
+			expected = false;
+		}
+	}
+	void unlock() {
+		m_locked.store(false);
+	}
+
+private:
+	atomic_bool m_locked = false;
+};
+
+mutex m;
+int32 sum;
+SpinLock spinLock;
+
+void Add()
+{
+	for (int32 i = 0; i < 100000; ++i) {
+		lock_guard<SpinLock> g(spinLock);
+		++sum;
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 100000; ++i) {
+		lock_guard<SpinLock> g(spinLock);
+		--sum;
+	}
+}
+
 int main()
 {
-    HelloWorld();
+	thread t1(Add);
+	thread t2(Sub);
+	t1.join(); t2.join();
+
+	cout << sum << endl;
 }
