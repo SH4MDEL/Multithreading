9bd0dc2e4a05aa3aef8358565d76ff2c57bde1d8
Seungjun Lee <beeutiful@naver.com>
2023-09-09 18:37:19 +0900

    조건 변수, future
    - 이벤트처럼 조건 변수를 통해 어떠한 조건이 충족되었음을 하나의 쓰레드 혹은 여러 쓰레드에 알릴 수 있다. 쓰레드는 조건을 확인하여 내가 깨어날지, 다시 잘지 선택할 수 있다.
    - Future를 사용하여 미래에 반환됨이 보장된 변수(promise) 또는 실행됨이 보장된 함수(packaged_task)의 값을 받아 올 수 있다. 또는 async를 통해 내부적으로 쓰레드를 생성할 수도 있다.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,28 +5,25 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
+
+mutex m;
+queue<int32> q;
+condition_variable cv;
 
 class SpinLock
 {
 public:
 	void lock() {
-		// CAS (Compare And Swap)
-
-		//if (m_locked == expected) {
-		//	m_locked = desired;
-		//	return true;
-		//}
-		//else {
-		//	expected = m_locked;
-		//	return false;
-		//}
-
 		bool expected = false;
 		const bool desired = true;
 
 		while (m_locked.compare_exchange_strong(expected, desired) == false) 
 		{
 			expected = false;
+
+			this_thread::sleep_for(std::chrono::milliseconds(100));
+			//this_thread::yield();
 		}
 	}
 	void unlock() {
@@ -37,31 +34,127 @@ private:
 	atomic_bool m_locked = false;
 };
 
-mutex m;
-int32 sum;
-SpinLock spinLock;
-
-void Add()
+void Producer()
 {
-	for (int32 i = 0; i < 100000; ++i) {
-		lock_guard<SpinLock> g(spinLock);
-		++sum;
+	while (true)
+	{
+		// 1) Lock을 잡음
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 품
+		// 4) 조건 변수를 통해 다른 쓰레드에게 통지
+		{
+			unique_lock<mutex> lock(m);
+			q.push(100);
+		}
+
+		cv.notify_one(); // wait 중인 쓰레드가 있으면 딱 하나를 깨운다.
 	}
 }
 
-void Sub()
+void Consumer()
 {
-	for (int32 i = 0; i < 100000; ++i) {
-		lock_guard<SpinLock> g(spinLock);
-		--sum;
+	while (true)
+	{
+		unique_lock<mutex> lock(m);
+		cv.wait(lock, []() {
+			return !q.empty();
+			});
+		// 1) Lock을 잡으려고 시도하고
+		// 2) 조건 확인
+		// - 만족할 경우 빠져 나와서 코드 진행
+		// - 만족하지 않을 경우 Lock을 풀어주고 대기 상태
+
+		// 그런데 notify_one을 했으면 항상 조건식을 만족하는 것 아닐까?
+		// Squrious Wakeup
+		// notify_one 할 때 lock을 잡고 있는 것이 아니기 때문에 다시 확인한다.
+
+		{
+			int32 data = q.front();
+			q.pop();
+			cout << q.size() << endl;
+		}
 	}
+}
+
+int64 result;
+
+int64 Calculate()
+{
+	int64 sum = 0;
+
+	for (int32 i = 0; i < 100'000; ++i) {
+		sum += i;
+	}
+	return sum;
+}
+
+void PromiseWorker(std::promise<string>&& promise)
+{
+	promise.set_value("Secret Message");
+}
+
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
-	thread t1(Add);
-	thread t2(Sub);
-	t1.join(); t2.join();
-
+	// 동기 실행 (synchronous) 실행
+	int64 sum = Calculate();
 	cout << sum << endl;
+
+	// std::future
+	{
+		// 1) deferred -> lazy evaluation : 지연해서 실행하세요
+		// 2) async -> 별도의 쓰레드를 만들어서 실행하세요
+		// 3) deferred | async -> 둘 중 알아서 골라주세요
+
+		// 언젠가 미래에 결과물을 뱉어줄거야
+		std::future<int64> future = std::async(std::launch::async, Calculate);
+
+		// TODO
+		std::future_status status = future.wait_for(1ms);
+		if (status == future_status::ready) {
+
+		}
+
+		int64 sum = future.get();
+
+		class Knight
+		{
+		public:
+			int64 GetHp() { return 100; }
+		};
+		Knight knight;
+		//std::future<int64> future = std::async(std::launch::async, &Knight::GetHp, knight);
+
+	}
+
+	// std::promise
+	{
+		// 미래(std::future)에 결과물을 반환해줄거라 약속
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		thread t(PromiseWorker, move(promise));
+
+		string message = future.get();
+		cout << message << endl;
+
+		t.join();
+	}
+
+	// std::packaged_task
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		thread t(TaskWorker, move(task));
+
+		int64 sum = future.get();
+		cout << sum << endl;
+
+		t.join();
+	}
 }
